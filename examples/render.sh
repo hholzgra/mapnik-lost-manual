35d11c78c113e16996c6a4427e15060a3338791f
Hartmut Holzgraefe <hartmut@php.net>
2022-11-27 15:35:05 +0100

    keep original PNG when trimming fails

--- examples/render.sh
+++ examples/render.sh
@@ -6,5 +6,8 @@ tmpnam=$(basename $(mktemp -p . tmprenderXXXXXXXXXX))
 cp $dir/$base.xml $tmpnam.xml
 ./render.py $tmpnam
 mv $tmpnam.svg $dir/$base.svg
-convert -trim $tmpnam.png $dir/$base.png
+if ! convert -regard-warnings -trim $tmpnam.png $dir/$base.png
+then
+	cp $tmpnam.png $dir/$base.png
+fi
 rm -f $tmpnam $tmpnam.png $tmpnam.xml
